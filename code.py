165cd6d67c451940ca6637642a30ee3ed02206c6
Tom Anderson <tomacorp@gmail.com>
2019-12-21 22:06:27 -0800

    Added full MIT license text

--- code.py
+++ code.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2019 Tom Anderson
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # Tom Anderson
 # Tue Nov 19 20:25:05 PST 2019
 # Sat Dec 21 18:57:39 PST 2019
@@ -467,17 +487,14 @@ class DisplayView(object):
                 horizontal_counter -= 1
             sample_index += 1
 
-
-
 class LedView(object):
-    """Class to operate the NeoPixels"""
+    """Class to turn the NeoPixels on and off"""
     
     import neopixel
     
     PIXEL_SWEEP = const(0)
     PIXEL_SELECT = const(2)
     PIXEL_REFRESH = const(4)
-
     
     def __init__(self, leds):
         """ Neopixel initialization """
@@ -580,5 +597,3 @@ while(True):
     # Erase the waveform by redrawing the pixels with black.
     screen.draw_trace(0, channel)
     lights.set_light_color(LedView.PIXEL_REFRESH, 'black')
-    
-    
